0ed4a37226813055497b2f9cf6558de04551707d
Budderstock <shanejanish@gmail.com>
2022-09-01 09:22:45 -0500

    changse

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -1,6 +1,6 @@
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.println("Shane is the GOAT!");
+        System.out.print("Shane is the GOAT! ");
         System.out.println("Ephraim is the biggest L of all time.");
     }
 }
